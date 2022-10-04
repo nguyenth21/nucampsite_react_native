c38da3cec916e2a738a54d53b7a2fb06aacfacb4
Thanh Nguyen <nguyenth21@gmail.com>
2022-10-03 21:34:27 -0500

    Icons, Favorites, and Comments

---
+++ screens/AboutScreen.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import { ScrollView, Text } from "react-native";
+import { Avatar, Card, ListItem } from "react-native-elements";
+import { PARTNERS } from "../shared/partners";
+
+const AboutScreen = () => {
+  const [partners, setPartners] = useState(PARTNERS);
+
+  return (
+    <ScrollView>
+      <Mission />
+      <Partner />
+    </ScrollView>
+  );
+};
+
+const Mission = () => {
+  return (
+    <ScrollView>
+      <Card>
+        <Card.Title>Out Mission</Card.Title>
+        <Card.Divider />
+        <Text style={{ margin: 10 }}>
+          We present a curated database of the best campsites in the vast woods
+          and backcountry of the World Wide Web Wilderness. We increase access
+          to adventure for the public while promoting safe and respectful use of
+          resources. The expert wilderness trekkers on our staff personally
+          verify each campsite to make sure that they are up to our standards.
+          We also present a platform for campers to share reviews on campsites
+          they have visited with each other.
+        </Text>
+      </Card>
+    </ScrollView>
+  );
+};
+
+const Partner = () => {
+  return (
+    <ScrollView>
+      <Card>
+        <Card.Title>Community Partners</Card.Title>
+        <Card.Divider />
+        {PARTNERS.map((partner) => {
+          return (
+            <ListItem key={partner.id}>
+              <Avatar source={partner.image} rounded />
+              <ListItem.Content>
+                <ListItem.Title>{partner.name}</ListItem.Title>
+                <ListItem.Subtitle>{partner.description}</ListItem.Subtitle>
+              </ListItem.Content>
+            </ListItem>
+          );
+        })}
+      </Card>
+    </ScrollView>
+  );
+};
+
+export default AboutScreen;
