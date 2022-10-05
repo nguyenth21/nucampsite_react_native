3460496d5d1277f7084ae5675c56a731c1fe51c0
Thanh Nguyen <nguyenth21@gmail.com>
2022-10-04 21:51:20 -0500

    Using Redux

--- redux/store.js
+++ redux/store.js
@@ -1,14 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { campsitesReducer } from '../features/campsites/campsitesSlice';
-import { commentsReducer } from '../features/comments/commentsSlice';
-import { partnersReducer } from '../features/partners/partnersSlice';
-import { promotionsReducer } from '../features/promotions/promotionsSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { campsitesReducer } from "../features/campsites/campsitesSlice";
+import { commentsReducer } from "../features/comments/commentsSlice";
+import { partnersReducer } from "../features/partners/partnersSlice";
+import { promotionsReducer } from "../features/promotions/promotionsSlice";
 
 export const store = configureStore({
-    reducer: {
-        campsites: campsitesReducer,
-        comments: commentsReducer,
-        partners: partnersReducer,
-        promotions: promotionsReducer
-    }
+  reducer: {
+    campsites: campsitesReducer,
+    comments: commentsReducer,
+    partners: partnersReducer,
+    promotions: promotionsReducer,
+  },
 });
